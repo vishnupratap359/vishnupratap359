a4c0df5c17ba0cf6cf70aba4dce41f42140e0085
vishnupratap359 <101706976+vishnupratap359@users.noreply.github.com>
2022-03-30 02:01:44 +0530

    find vowel

---
+++ VOWEL.C
@@ -0,0 +1,17 @@
+#include<stdio.h>
+#include<conio.h>
+void main()
+{
+ char ch;
+ clrscr();
+ printf("enter the alphabets :-");
+ scanf("%c", ch);
+ if(ch=='a' || ch=='e' || ch=='i' || ch== 'o' || ch=='u')
+ printf("\n it's a vowel");
+ else if (ch=='A' || ch=='E' || ch=='I' || ch=='O' || ch=='U')
+ printf("\n it's a vowel");
+ else
+  printf("\n it's a consonant");
+       getch();
+       }
+
